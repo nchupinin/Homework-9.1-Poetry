7231fb82c1dd64c620eba5bc138fc677c202c0c5
Nicolay Chupinin <nicochup20012018@gmail.com>
2025-04-07 17:12:19 +0300

    Добавил новую функцию, которая возвращает новый список, отсортированный по дате. True (по умолчанию), сортирует по убыванию от новых к старым.

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,9 @@
-def  filter_by_state(list_data: list) -> list:
-    '''Возвращает список словарей, где state == 'EXECUTED'.'''
+from datetime import datetime
+from typing import Dict, List
+
+
+def filter_by_state(list_data: list) -> list:
+    """Возвращает список словарей, где state == 'EXECUTED'."""
 
     new_list = []
     for sublist in list_data:
@@ -9,8 +13,9 @@ def  filter_by_state(list_data: list) -> list:
     return new_list
 
 
-# def sort_by_date(list_data: list) -> list:
-#     '''Возвращает новый список, отсортированный по дате'''
+def sort_by_date(list_data: list[dict], reverse: bool = True) -> list[dict]:
+    """Возвращает новый список, отсортированный по дате
+    True (по умолчанию), сортирует по убыванию (новые → старые)."""
 
-
-
+    flat_list = [item for sublist in list_data for item in sublist]
+    return sorted(flat_list, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
