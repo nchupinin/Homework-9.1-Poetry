5e8ecf06a374d828fef7e5f95931384d2304aec7
Nicolay Chupinin <nicochup20012018@gmail.com>
2025-04-24 09:31:07 +0300

    Доработка домашней работы

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Any, Dict, List
 
 from src.processing import filter_by_state, sort_by_date
-from src.widget.mask_account_card import get_date, get_mask_account, get_mask_card_number
+from src.masks import get_mask_card_number
+from src.widget import get_date, get_mask_account
 
 
 def main() -> None:
--- src/widget.py
+++ src/widget.py
@@ -1,22 +1,4 @@
-def get_mask_card_number(card_number: str) -> str:
-    """Маскирует номер карты в формате 'XXXX XX** **** XXXX'"""
 
-    if card_number is None:
-        raise ValueError("Номер карты не может быть None")
-
-    if card_number == "":
-        raise ValueError("Номер карты не может быть пустым")
-
-    if not isinstance(card_number, str):
-        raise ValueError("Номер карты должен быть строкой")
-
-    if not card_number.isdigit():
-        raise ValueError("Номер карты должен содержать только цифры")
-
-    if len(card_number) != 16:
-        raise ValueError("Требуется строка из 16 цифр")
-
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
